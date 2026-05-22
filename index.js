db37de63633b36ff461f8d50987709ee47f83b4d
Rasel Ahmed <raselahmediu22@gmail.com>
2026-05-22 20:36:30 +0600

    google login issue

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const app = express()
 const PORT = process.env.PORT || 8000
 const uri = process.env.MONGODB_URI
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://sports-nest-gules.vercel.app',
+  credentials: true,
+}))
 
 app.use(express.json())
 
