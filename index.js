680f993e782e8cbc4e456d585acffed2c82abae1
Rasel Ahmed <raselahmediu22@gmail.com>
2026-05-22 09:54:34 +0600

    vercel

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const client = new MongoClient(uri, {
 async function run() {
 
   try {
-    // await client.connect()
+    await client.connect()
     const db = client.db('sportsNest')
     const facilitiesCollection = db.collection('facilities')
     const bookingsCollection = db.collection('bookings')
